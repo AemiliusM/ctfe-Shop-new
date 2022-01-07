6e6e486ab0b0e9815fbecafca98e6f118ec0dcf9
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-01-07 12:34:39 -0800

    took out app.css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css';
 import Shop from './views/Shop';
 
 function App() {
   return (
-      <main className='App-main'>
-        <Shop />
-      </main>
+    <main className="App-main">
+      <Shop />
+    </main>
   );
 }
 
