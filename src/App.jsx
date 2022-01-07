fdfa950e14b4ae2b2352fb46ff14f9c6c6ba87a9
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-01-07 12:30:21 -0800

    adjusted app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1>Hello World</h1>;
+import './App.css';
+import Shop from './views/Shop';
+
+function App() {
+  return (
+      <main className='App-main'>
+        <Shop />
+      </main>
+  );
 }
+
+export default App;
